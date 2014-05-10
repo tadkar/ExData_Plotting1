f91cd202353e0cccf60c1a9a93e1d9abdddb1cab
tadkar <tadkar@yahoo.com>
2014-05-10 10:02:41 +0100

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,3 +1,12 @@
+##Read the data, use the option in read.csv to replace ? with NA
+
+powerData <- read.csv("~/Exploratory Analysis/household_power_consumption.txt", sep=";",na.strings=c("","?"))
+powerData$Date<-as.Date(powerData$Date,"%d/%m/%Y")
+
+## Pick the relevant subset to work with, *much* faster
+miniData<-powerData[powerData$Date %in% as.Date(c("2007-02-01","2007-02-02"))]
+
+##create a helper variable with all the required date and time info in one variable
 datetime<-as.POSIXct(paste(miniData$Date," ",miniData$Time),format="%Y-%m-%d %H:%M:%S")
 
 plot(datetime,miniData$Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)")
