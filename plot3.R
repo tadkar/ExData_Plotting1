b9b006e7a97a8314888e1462e3a1cd6674d4f567
tadkar <tadkar@yahoo.com>
2014-05-10 10:06:20 +0100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -6,3 +6,18 @@ powerData$Date<-as.Date(powerData$Date,"%d/%m/%Y")
 ## Pick the relevant subset to work with, *much* faster
 miniData<-powerData[powerData$Date %in% as.Date(c("2007-02-01","2007-02-02"))]
 
+## because the x values are all the same just add lines
+## found this website helpful
+## http://cran.r-project.org/doc/contrib/Lemon-kickstart/kr_addat.html
+
+##Plot the 3 series on the same graph
+plot(datetime,miniData$Sub_metering_1,type="l",xlab="",ylab="Energy sub metering")
+lines(datetime,miniData$Sub_metering_2,col="red")
+lines(datetime,miniData$Sub_metering_3,col="blue")
+## the lwd option is important, it seems that the default option is a line width of 0 which doesn't show up
+legend("topright",title="",legend=names(miniData)[grep("Sub*",names(miniData))], seg.len=2.0, lwd=1,col=c("black","red","blue"))
+
+## Write output file
+## default option is 480x480
+dev.copy(png,"plot3.png")
+dev.off()
